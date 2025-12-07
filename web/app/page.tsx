612a4f56cd3df595f7dcddbeeeaed37888a2e5b8
Evan Sayani <easayani@gmail.com>
2025-12-07 00:05:09 -0500

    Update Plasmic component usage

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,13 +2,13 @@ import { PlasmicComponent } from '@plasmicapp/loader-nextjs'
 import { PLASMIC } from '@/plasmic-init'
 
 export default async function Home() {
-  // Preload Plasmic data
+  // Preload Plasmic data for the component
   const plasmicData = await PLASMIC.fetchComponentData('HomePage')
   
   return (
-    <PlasmicComponent 
+    <PlasmicComponent
+      loader={PLASMIC}
       component="HomePage"
-      componentProps={plasmicData}
     />
   )
 }
